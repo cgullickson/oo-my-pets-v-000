a21eb3a0dcbbf95905c880dc94eab98049c28ca5
Chris Gullickson <cgullickson5@gmail.com>
2017-12-07 21:01:03 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,9 +2,13 @@ class Owner
   attr_reader :name
   attr_accessor :mood
 
+  @@all = []
+  @@count = 0
+
   def initialize (name, mood = "nervous")
     @name = name
     @mood = mood
+    @@count += 1
   end
   # code goes here
 end

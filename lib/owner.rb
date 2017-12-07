1264fcf06abd296d6986d8f34a8f6ea57f472ab5
Chris Gullickson <cgullickson5@gmail.com>
2017-12-07 21:49:51 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -52,7 +52,15 @@ class Owner
   end
 
   def walk_dogs
-    self.pets[:dogs].mood= "happy"
+    self.pets[:dogs].each {|dog| dog.mood = "happy"}
+  end
+
+  def play_with_cats
+    self.pets[:cats].each {|cat| cat.mood = "happy"}
+  end
+
+  def feed_fish
+    self.pets[:fishes].each {|fish| fish.mood = "happy"}
   end
 
   # code goes here
